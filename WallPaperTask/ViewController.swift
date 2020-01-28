e1d6f2a6b947499da852dc990be675fbae9b8550
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-01-28 14:49:10 +0530

    Initial Commit

---
+++ WallPaperTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallPaperTask
+//
+//  Created by Vadde Narendra on 1/28/20.
+//  Copyright © 2020 Vadde Narendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
